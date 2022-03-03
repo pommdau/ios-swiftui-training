48554a6be55a332abdcb5face9a7b9ea11d022ff
IKEH1024 <pondau1024@gmail.com>
2022-03-03 09:31:16 +0900

    keep reading

--- Section1/GitHubClient/GitHubClient/Views/RepoListView.swift
+++ Section1/GitHubClient/GitHubClient/Views/RepoListView.swift
@@ -9,21 +9,38 @@ import SwiftUI
 
 struct RepoListView: View {
     
-    private let mockRepos: [Repo] = [
-        .mock1, .mock2, .mock3, .mock4, .mock5
-    ]
+    // MARK: - Properties
+    
+    @State private var mockRepos: [Repo] = []
+    
+    // MARK: - Views
     
     var body: some View {
         NavigationView {
             List(mockRepos) { repo in
                 NavigationLink(
                     destination: RepoDetailView(repo: repo)) {
-                    RepoRow(repo: repo)
-                }
+                        RepoRow(repo: repo)
+                    }
             }
             .navigationTitle("Repositories")
+            .onAppear {
+                loadRepos()
+            }
         }
     }
+    
+    // MARK: - Helpers
+    
+    private func loadRepos() {
+        // 1秒後にモックデータを読み込む
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            mockRepos = [
+                .mock1, .mock2, .mock3, .mock4, .mock5
+            ]
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
